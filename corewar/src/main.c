fadeca209fff720f657a1c6956e20efad5b06371
victor sousa <victor.sousa@epitech.eu>
2016-02-29 22:38:45 +0100

    [*] created corewar folder

---
+++ corewar/src/main.c
@@ -0,0 +1,16 @@
+/*
+** main.c for corewar in /home/vicostudio/rendu/cpe/CorewarGIT/corewar
+**
+** Made by victor sousa
+** Login   <sousa_v@epitech.net>
+**
+** Started on  Mon Feb 29 22:21:44 2016 victor sousa
+** Last update Mon Feb 29 22:22:02 2016 victor sousa
+*/
+
+#include	"corewar.h"
+
+int		main(void)
+{
+  return (0);
+}
---
+++ includes/includes_corewar/corewar.h
@@ -0,0 +1,16 @@
+/*
+** corewar.h for corewar in /home/vicostudio/rendu/cpe/CorewarGIT/corewar
+**
+** Made by victor sousa
+** Login   <sousa_v@epitech.net>
+**
+** Started on  Mon Feb 29 22:25:56 2016 victor sousa
+** Last update Mon Feb 29 22:26:32 2016 victor sousa
+*/
+
+#ifndef			COREWAR_H_
+# define		COREWAR_H_
+
+
+
+#endif		      /*COREWAR_H*/
