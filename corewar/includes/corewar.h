d008518963b1e53c223855d06148fd27f53c1096
victor sousa <victor.sousa@epitech.eu>
2016-03-07 00:23:14 +0100

    [*] some error output fonction

--- corewar/includes/corewar.h
+++ corewar/includes/corewar.h
@@ -5,14 +5,14 @@
 ** Login   <sousa_v@epitech.net>
 **
 ** Started on  Mon Feb 29 22:25:56 2016 victor sousa
-** Last update Mon Mar  7 00:11:47 2016 Victor Sousa
+** Last update Mon Mar  7 00:22:01 2016 Victor Sousa
 */
 
 #ifndef			COREWAR_H_
 # define		COREWAR_H_
 
+# include		<stdlib.h>
 # include		<unistd.h>
-# include		<stdio.h>
 
 # include		"op.h"
 # include		"utils.h"
--- corewar/src/main.c
+++ corewar/src/main.c
@@ -5,7 +5,7 @@
 ** Login   <sousa_v@epitech.net>
 **
 ** Started on  Mon Feb 29 22:21:44 2016 victor sousa
-** Last update Mon Mar  7 00:13:40 2016 Victor Sousa
+** Last update Mon Mar  7 00:22:29 2016 Victor Sousa
 */
 
 #include	"corewar.h"
@@ -17,7 +17,7 @@ int		main(int ac, char **av, char **env)
   (void)champion;
   if (env != NULL && verif_arg(ac, av) == 0)
     {
-      my_putstr("programm failed\n");
+      my_putstr_error("programm failed\n", 1);
       return (1);
     }
   my_putstr("program running\n");
