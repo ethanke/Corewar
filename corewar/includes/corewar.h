a90ba3966ca3430c26c5594b790a171e2d390858
victor sousa <victor.sousa@epitech.eu>
2016-03-06 23:39:43 +0100

    [-] moved the .h in corewar folder TROP CHIANT.. bon on va essayer de garder des commit reglo..

--- corewar/includes/corewar.h
+++ corewar/includes/corewar.h
@@ -5,13 +5,14 @@
 ** Login   <sousa_v@epitech.net>
 **
 ** Started on  Mon Feb 29 22:25:56 2016 victor sousa
-** Last update Sun Mar  6 22:33:06 2016 Gaëtan Léandre
+** Last update Sun Mar  6 23:36:08 2016 victor sousa
 */
 
 #ifndef			COREWAR_H_
 # define		COREWAR_H_
 
 # include		<unistd.h>
+
 # include		"op.h"
 # include		"champ.h"
 
