91b424c9c6dc3017e3b9580ca9dbaeae4f94d99c
leandr_g <gaetan.leandre@epitech.eu>
2016-03-07 00:31:33 +0100

    [*]arena : ajout dans le corewar.h

--- corewar/includes/corewar.h
+++ corewar/includes/corewar.h
@@ -5,7 +5,7 @@
 ** Login   <sousa_v@epitech.net>
 **
 ** Started on  Mon Feb 29 22:25:56 2016 victor sousa
-** Last update Mon Mar  7 00:22:01 2016 Victor Sousa
+** Last update Mon Mar  7 00:31:14 2016 Gaëtan Léandre
 */
 
 #ifndef			COREWAR_H_
@@ -17,6 +17,7 @@
 # include		"op.h"
 # include		"utils.h"
 # include		"champ.h"
+# include		"arena.h"
 # include		"output.h"
 
 #endif		      /*COREWAR_H*/
