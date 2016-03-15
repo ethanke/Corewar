db5be70166289cc3358a5a44e580b60b2bb69bfc
leandr_g <gaetan.leandre@epitech.eu>
2016-03-15 22:25:31 +0100

    [*]ld implementee dans le makefile

--- corewar/src/funct/ld.c
+++ corewar/src/funct/ld.c
@@ -5,51 +5,49 @@
 ** Login   <leandr_g@epitech.eu>
 **
 ** Started on  Fri Mar 11 00:34:17 2016 Gaëtan Léandre
-** Last update Sun Mar 13 12:37:49 2016 Gaëtan Léandre
+** Last update Tue Mar 15 22:23:24 2016 Gaëtan Léandre
 */
 
 #include	"corewar.h"
 
 int		my_ld(t_arena *arena, t_process *process, int id, int pc_pos)
 {
-  int		arg;
+  unsigned char	arg;
   int		nbr;
   int		reg;
-  int		size;
 
+  (void)id;
   process->pos = circle(process->pos, 1);
-  if (check_mult_args(arena->arena, 2, process->pos, 2) == -1)
+  arg = arena->arena[process->pos];
+  if (check_mult_args(arena->arena, 1, process->pos, 2) == -1)
     return (0);
-  process->cycle += op_tab[2].nbr_cycles;
+  process->cycle += op_tab[1].nbr_cycles;
   process->carry = (process->carry == 1) ? 0 : 1;
-  nbr = take_what(arena->arena, process->pos, 1, pc_pos);
-  reg = octet_to_read(arena->arena[process->pos]);
-  if ((size = check_reg(take_param(arena->arena, process->pos + reg + 2,
-				   REG_SIZE))) == -1)
-    return (0);
-  arena->arena = put_param(arena->arena, pc_pos + 1 + (size - 1) * REG_SIZE, REG_SIZE, nbr);
-  process->pos = circle(process->pos, 2 + reg + REG_SIZE);
+  nbr = take_what(arena->arena, process->pos, make_choix(pc_pos, 1, 1),
+		  process->reg);
+  reg = arena->arena[circle(process->pos, octet_to_read(arg, 1) + 1)];
+  process->reg[reg - 1] = nbr;
+  process->pos = circle(process->pos, place_to_jump(arg, 2) + 1);
   return (0);
 }
 
 int		my_lld(t_arena *arena, t_process *process, int id, int pc_pos)
 {
-  int		arg;
+  unsigned char	arg;
   int		nbr;
   int		reg;
-  int		size;
 
+  (void)id;
   process->pos = circle(process->pos, 1);
-  if (check_mult_args(arena->arena, 2, process->pos, 2) == -1)
+  arg = arena->arena[process->pos];
+  if (check_mult_args(arena->arena, 12, process->pos, 2) == -1)
     return (0);
-  process->cycle += op_tab[2].nbr_cycles;
+  process->cycle += op_tab[12].nbr_cycles;
   process->carry = (process->carry == 1) ? 0 : 1;
-  nbr = take_what(arena->arena, process->pos, 0, pc_pos);
-  reg = octet_to_read(arena->arena[process->pos]);
-  if ((size = check_reg(take_param(arena->arena, process->pos + reg + 2,
-				   REG_SIZE))) == -1)
-    return (0);
-  arena->arena = put_param(arena->arena, pc_pos + 1 + (size - 1) * REG_SIZE, REG_SIZE, nbr);
-  process->pos = circle(process->pos, 2 + reg + REG_SIZE);
+  nbr = take_what(arena->arena, process->pos, make_choix(pc_pos, 0, 1),
+		  process->reg);
+  reg = arena->arena[circle(process->pos, octet_to_read(arg, 1) + 1)];
+  process->reg[reg] = nbr;
+  process->pos = circle(process->pos, place_to_jump(arg, 2));
   return (0);
 }
