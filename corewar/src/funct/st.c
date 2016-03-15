186d2aee311d6f738a367c1c1e4c035df03af2a5
leandr_g <gaetan.leandre@epitech.eu>
2016-03-16 00:02:12 +0100

    [*]st.c ajoute au makefile avec quelques petites remises a niveau

--- corewar/src/funct/st.c
+++ corewar/src/funct/st.c
@@ -5,7 +5,7 @@
 ** Login   <leandr_g@epitech.eu>
 **
 ** Started on  Sun Mar 13 11:45:09 2016 Gaëtan Léandre
-** Last update Sun Mar 13 12:37:46 2016 Gaëtan Léandre
+** Last update Wed Mar 16 00:00:54 2016 Gaëtan Léandre
 */
 
 #include	"corewar.h"
@@ -13,51 +13,48 @@
 int		my_st(t_arena *arena, t_process *process, int id, int pc_pos)
 {
   int		nbr;
-  int		reg;
   int		ind;
+  unsigned char	arg;
 
+  (void)id;
   process->pos = circle(process->pos, 1);
-  if (check_mult_args(arena->arena, 3, process->pos, 2) == -1)
+  arg = arena->arena[process->pos];
+  if (check_mult_args(arena->arena, 2, process->pos, 2) == -1)
     return (0);
-  process->cycle += op_tab[3].nbr_cycles;
-  nbr = take_what(arena->arena, process->pos, 1, pc_pos);
-  reg = octet_to_read(arena->arena[process->pos]);
-  if (arena->arena[process->pos + reg + 1] == T_REG)
-    {
-      ind = take_param(arena->arena, process->pos + reg + 2, REG_SIZE);
-      arena->arena = put_param(arena->arena, pc_pos + 1 + (ind - 1)
-			       * REG_SIZE, REG_SIZE, nbr);
-    }
+  process->cycle += op_tab[2].nbr_cycles;
+  nbr = process->reg[arena->arena[circle(process->pos, 1)] - 1];
+  if (octet_to_read(arg, 1) == T_REG)
+    process->reg[arena->arena[circle(process->pos, 2)] - 1] = nbr;
   else
     {
-      ind = take_param(arena->arena, process->pos + reg + 2, IND_SIZE);
+      ind = take_param(arena->arena, process->pos + 2, IND_SIZE);
       arena->arena = put_param(arena->arena,
-			       circle(pc_pos + mod(ind, IDX_MOD), 0)
+			       circle(pc_pos, mod(ind, IDX_MOD))
 			       * REG_SIZE, DIR_SIZE, nbr);
     }
-  process->pos = circle(process->pos, 2 + reg +
-			octet_to_read(arena->arena[process->pos + reg + 1]));
+  process->pos = circle(process->pos, place_to_jump(arg, 2) + 1);
   return (0);
 }
 
 int		my_sti(t_arena *arena, t_process *process, int id, int pc_pos)
 {
-  int		nbr;
-  int		reg;
-  int		regB;
-  int		ind;
+  int		a;
+  int		b;
+  int		c;
+  unsigned char	arg;
 
+  (void)id;
   process->pos = circle(process->pos, 1);
+  arg = arena->arena[process->pos];
   if (check_mult_args(arena->arena, 11, process->pos, 3) == -1)
     return (0);
   process->cycle += op_tab[11].nbr_cycles;
-  nbr = take_what(arena->arena, process->pos, 1, pc_pos);
-  reg = octet_to_read(arena->arena[process->pos]);
-  ind = take_what(arena->arena, process->pos + reg + 1, 1, pc_pos);
-  regB = octet_to_read(arena->arena[process->pos]);
-  ind += take_what(arena->arena, process->pos + reg + regB + 2, 1, pc_pos);
-  arena->arena = put_param(arena->arena, circle(ind, 0) * REG_SIZE, DIR_SIZE, nbr);
-  process->pos = circle(process->pos, 3 + reg + regB +
-			octet_to_read(arena->arena[process->pos + reg + regB + 2]));
+  a = process->reg[arena->arena[circle(process->pos, 1)] - 1];
+  b = take_what(arena->arena, process->pos, make_choix(pc_pos, 0, 2),
+		process->reg);
+  c = take_what(arena->arena, process->pos, make_choix(pc_pos, 0, 3),
+		process->reg);
+  arena->arena = put_param(arena->arena, circle(pc_pos, b + c), REG_SIZE, a);
+  process->pos = circle(process->pos, place_to_jump(arg, 3) + 1);
   return (0);
 }
